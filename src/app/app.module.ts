e68755e482aaaf34bde2a786ca8ef5e964ce6b83
yosra mekaoui <61566287+yosra-mekaoui@users.noreply.github.com>
2022-12-03 14:53:42 +0100

    CrudExperience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ExperienceModule } from './experience/experience.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     ExperienceModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/experience/list-experience/list-experience.component.ts
+++ src/app/experience/list-experience/list-experience.component.ts
@@ -1,7 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+
 import { Router } from '@angular/router';
+
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Experience } from 'src/app/Models/Experience';
+import { DialogExperienceComponent } from '../dialog-experience/dialog-experience.component';
+
 import { ServiceExperienceService } from 'src/app/service-experience.service';
+
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+
 
 @Component({
   selector: 'app-list-experience',
@@ -9,35 +20,117 @@ import { ServiceExperienceService } from 'src/app/service-experience.service';
   styleUrls: ['./list-experience.component.css']
 })
 export class ListExperienceComponent implements OnInit {
-  
-  constructor(private serviceExperience:ServiceExperienceService,private router:Router) { }
-  list:Experience[]=[]
+  displayedColumns: string[] = ['idExperience','type','titreDuProfil','dateDebutExperience','dateFinExperience','descriptif','lieu','actions'];
+  dataSource!: MatTableDataSource<any>;
 
-  ngOnInit(): void {
-    this.getListExperience();
-  }
-  getListExperience(){
-    this.serviceExperience.getData().subscribe(
-      data=>{this.list=data
-      console.log(this.list)}
-    )
-      
-  }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  deleteexperience(id: number){
-    this.serviceExperience.deleteExperience(id).subscribe( data => {
-      console.log(data);
-      this.getListExperience();
-    })
-  }
-  public onOpenModal(experience: Experience, mode: string): void{
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.style.display = 'none';
-    button.setAttribute('data-toggle', 'modal');
-    if(mode === 'add'){
-      button.setAttribute('data-target', 'modal');
+  constructor(public dialog: MatDialog,
+    private experienceService:ServiceExperienceService) {}
 
-    }
-  }
+openDialog() {
+this.dialog.open(DialogExperienceComponent, {
+width:'30%'
+
+}).afterClosed().subscribe(val=>{
+if(val==='ajout'){
+this.getAllExperiences()
+}
+});
+}
+
+ngOnInit(): void {
+this.getAllExperiences()
+
+}
+
+
+
+
+
+
+getAllExperiences(){
+this.experienceService.getExperience()
+.subscribe({
+next: (res)=>{
+console.log(res);
+this.dataSource=new MatTableDataSource(res)
+console.log("heeeelooo");
+
+this.dataSource.paginator=this.paginator
+this.dataSource.sort=this.sort
+
+},
+error:()=>{
+alert("erreur get all")
+}
+
+
+})
+
+
+
+}
+
+
+applyFilter(event: Event) {
+const filterValue = (event.target as HTMLInputElement).value;
+this.dataSource.filter = filterValue.trim().toLowerCase();
+
+if (this.dataSource.paginator) {
+this.dataSource.paginator.firstPage();
+}
+}
+
+
+
+editExperience(row :any) {
+this.dialog.open(DialogExperienceComponent, {
+width:'30%',
+data:row
+
+}).afterClosed().subscribe(val=>{
+if(val==='update'){
+this.getAllExperiences()
+}
+});;
+}
+
+
+
+
+
+/*deleteExperience(id:number){
+
+this.experienceService.deleteExperience(id)
+.subscribe({
+    next: (res)=>{
+alert("experience bien supprimer")
+this.getAllExperiences()
+
+},
+error:()=>{
+alert("erreur de suppression")
+}
+
+
+})
+
+}*/
+
+deleteExperience(id: number){
+  this.experienceService.deleteExperience(id).subscribe( data => {
+    alert("experience bien supprimer")
+
+    this.getAllExperiences()
+ 
+
+})
+}
+
+
+
+
+
 }
